c8175f95e9f22e91bf34281495f75dca8e93beee
Kunj Ghevariya <kunj.ghevariya@gmail.com>
2024-08-04 14:05:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,38 +1,16 @@
 import express from 'express';
-import fs from 'fs';
-import https from 'https';
+import http from 'http';
 import { Server as socketIo } from 'socket.io';
-import cors from 'cors';
 
 const app = express();
-const PORT = 8002;
-
-const isProduction = process.env.NODE_ENV === 'production';
-const server = isProduction
-  ? https.createServer({
-      key: fs.readFileSync('/path/to/your/production-key.pem'),
-      cert: fs.readFileSync('/path/to/your/production-cert.pem')
-    }, app)
-  : https.createServer({
-      key: fs.readFileSync('localhost-key.pem'),
-      cert: fs.readFileSync('localhost.pem')
-    }, app);
-
+const server = http.createServer(app);
 const io = new socketIo(server, {
   cors: {
-    origin: 'https://truthndare.netlify.app',
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
+    origin: "*",
+  },
 });
 
-const corsOptions = {
-  origin: 'https://truthndare.netlify.app',
-  methods: ['GET', 'POST'],
-  credentials: true
-};
 
-app.use(cors(corsOptions));
 
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -53,8 +31,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = 8002;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export { io };

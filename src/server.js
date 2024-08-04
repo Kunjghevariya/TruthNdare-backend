39f301ac64327cb6f5a83d84f1afc11a7b128dd7
Kunj Ghevariya <kunj.ghevariya@gmail.com>
2024-08-04 11:00:32 +0530

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,33 +1,60 @@
 import express from 'express';
-import http from 'http';
+import fs from 'fs';
+import https from 'https';
 import { Server as socketIo } from 'socket.io';
 import cors from 'cors';
 
 const app = express();
-const server = http.createServer(app);
-const io = new socketIo(server)
-  const corsOptions = {
+const PORT = 8002;
+
+const isProduction = process.env.NODE_ENV === 'production';
+const server = isProduction
+  ? https.createServer({
+      key: fs.readFileSync('/path/to/your/production-key.pem'),
+      cert: fs.readFileSync('/path/to/your/production-cert.pem')
+    }, app)
+  : https.createServer({
+      key: fs.readFileSync('localhost-key.pem'),
+      cert: fs.readFileSync('localhost.pem')
+    }, app);
+
+const io = new socketIo(server, {
+  cors: {
     origin: 'https://truthndare.netlify.app',
-  };
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
 
-  app.use(cors(corsOptions));
+const corsOptions = {
+  origin: 'https://truthndare.netlify.app',
+  methods: ['GET', 'POST'],
+  credentials: true
+};
 
-  io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('joinRoom', (roomID) => {
-      socket.join(roomID);
-      console.log(`User joined room: ${roomID}`);
-    });
-    socket.on('start', (roomID) => {
-      console.log(`Game started in room: ${roomID}`);
-      io.emit('start');
-      // io.to(roomID).emit('start');
-    });
-    socket.on('disconnect', () => {
-      console.log('A user disconnected');
-    });
+app.use(cors(corsOptions));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('joinRoom', (roomID) => {
+    socket.join(roomID);
+    console.log(`User joined room: ${roomID}`);
   });
-  const PORT = 8002;
-  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  export { io };
-  
+
+  socket.on('start', (roomID) => {
+    console.log(`Game started in room: ${roomID}`);
+    io.emit('start');
+    // io.to(roomID).emit('start');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export { io };

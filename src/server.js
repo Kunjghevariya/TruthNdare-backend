52bc5a55f666cf44439c41d232419378b6f5f56e
Kunj Ghevariya <kunj.ghevariya@gmail.com>
2024-08-04 10:48:50 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,37 +1,33 @@
 import express from 'express';
 import http from 'http';
 import { Server as socketIo } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
-const io = new socketIo(server, {
-  cors: {
-    origin: "https://truthndare.netlify.app",
-  },
-});
+const io = new socketIo(server)
+  const corsOptions = {
+    origin: 'https://truthndare.netlify.app',
+  };
 
+  app.use(cors(corsOptions));
 
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('joinRoom', (roomID) => {
-    socket.join(roomID);
-    console.log(`User joined room: ${roomID}`);
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('joinRoom', (roomID) => {
+      socket.join(roomID);
+      console.log(`User joined room: ${roomID}`);
+    });
+    socket.on('start', (roomID) => {
+      console.log(`Game started in room: ${roomID}`);
+      io.emit('start');
+      // io.to(roomID).emit('start');
+    });
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
   });
-
-  socket.on('start', (roomID) => {
-    console.log(`Game started in room: ${roomID}`);
-    io.emit('start');
-    // io.to(roomID).emit('start');
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
-
-const PORT = 8002;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-export { io };
+  const PORT = 8002;
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  export { io };
+  
